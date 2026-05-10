95fdb5b287499af6cf737e92893e5504b2ea5d3e
Trapti <traptivishwakarma2007@gmail.com>
2026-05-10 19:42:23 +0530

    fixed bugs

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
- 
+
 const api = axios.create({
   baseURL: "https://tektronics.businesssamadhan.in/api",
   headers: {
@@ -7,39 +7,39 @@ const api = axios.create({
     Accept: "application/json",
   },
 });
- 
+
+// REQUEST INTERCEPTOR
 api.interceptors.request.use(
   (config) => {
     const token =
       localStorage.getItem("token") ||
       sessionStorage.getItem("token");
- 
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
- 
+
     return config;
   },
   (error) => Promise.reject(error)
 );
- 
+
+// RESPONSE INTERCEPTOR
 api.interceptors.response.use(
   (response) => response,
+
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem("token");
-      localStorage.removeItem("user");
       sessionStorage.removeItem("token");
- 
-      // Avoid redirect loop if we're already on /login
-      if (!window.location.pathname.includes("/login")) {
-        alert("Session expired. Please login again.");
-        window.location.href = "/login";
-      }
+
+      alert("Session expired. Please login again.");
+
+      window.location.href = "/login";
     }
- 
+
     return Promise.reject(error);
   }
 );
- 
+
 export default api;
